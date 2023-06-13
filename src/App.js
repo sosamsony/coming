65a25d5363a4a768d9f697b182d96fdc658838a6
Samson Sanyaolu <sosarenaint@gmail.com>
2023-06-13 14:10:59 +0400

    Updated for Gulf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
-import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
+ // import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+ // import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
 import { ReactComponent as IconLinkedIn } from './assets/icons/linkedin.svg';
 import Logo from "../src/logo.png";
 
@@ -12,31 +12,25 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href="."><img src={Logo} alt="REDE LLC"></img></a>
+            <a href="."><img src={Logo} alt="GULF CREATIVE TEST GROUND"></img></a>
           </div>
           <div className="social">
-            <a href="https://facebook.com/redellc" title="Facebook" target="_blank" rel="noopener noreferrer">
-              <IconFacebook className="icon" />
-            </a>
-            <a href="https://twitter.com/redellc" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
-            </a>
-            <a href="https://linkedin/in/redellc" title="LinkedIn" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/company/gulf-creative-ae/" title="LinkedIn" target="_blank" rel="noopener noreferrer">
               <IconLinkedIn className="icon" />
             </a>
           </div>
         </div>
         <div className="content">
           <div className="title-holder">
-            <h1>We are ready for your technological services.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <h1>We are Gulf Creative, Dubai.</h1>
+            <p>Award winning marketing agency. This is our testing ground. Shoot us an email if you're curious.</p>
           </div>
-          <a href="mailto:info@werede.net">
+          <a href="mailto:hello@gulfcreative.ae">
             <div className="cta">Send us an email</div>
           </a>
         </div>
         <div className="footer">
-          <span>Check out our branding here...<a className="underlined" href="https://www.behance.net/gallery/163387547/REDE-LLC-Corporate-Branding" target="_blank" rel="noopener noreferrer">REDELLC</a> <br></br><strong>Powered by <a className="underlined" href="https://sosamson.com" title="Samson Sanyaolu" target="_blank" rel="noopener noreferrer">Samson Sanyaolu</a></strong></span>
+          <span>Check out our main website here...<a className="underlined" href="https://www.gulfcreative.ae" target="_blank" rel="noopener noreferrer">GULF CREATIVE</a> <br></br><strong>Powered by <a className="underlined" href="https://www.gulfcreative.ae" title="Gulf Creative" target="_blank" rel="noopener noreferrer">Gulf Creative</a></strong></span>
         </div>
       </div>
     );
